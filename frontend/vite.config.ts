ee3e1b03e73ab8aaadcc4d5f1c7f9d88f478c1de
Jacobtread <jacobtread@gmail.com>
2025-07-02 21:55:10 +1200

    feat: remove tailwind and setup mui

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,12 @@
 import { defineConfig } from "vite";
 import viteReact from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import { resolve } from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    tanstackRouter({ autoCodeSplitting: true }),
-    viteReact(),
-    tailwindcss(),
-  ],
+  plugins: [tanstackRouter({ autoCodeSplitting: true }), viteReact()],
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
